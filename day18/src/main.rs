e2816f4a69152a9d7446dad2d06c0a7cbe2c6ce2
Danny Yoo <dannyyoo@google.com>
2023-10-29 12:43:02 -0700

    Working on day18 part 1.

---
+++ day18/src/main.rs
@@ -0,0 +1,108 @@
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+struct Pos {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Pos {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        Pos { x, y, z }
+    }
+
+    fn faces(&self) -> Vec<Pos> {
+        let mut result = Vec::new();
+        for i in [-1, 1] {
+            result.push(Pos::new(self.x + i, self.y, self.z));
+        }
+        for j in [-1, 1] {
+            result.push(Pos::new(self.x, self.y + j, self.z));
+        }
+        for k in [-1, 1] {
+            result.push(Pos::new(self.x, self.y, self.z + k));
+        }
+        result
+    }
+}
+
+fn surface_area(cubes: &[Pos]) -> usize {
+    let possible_faces = cubes.iter().flat_map(Pos::faces).collect::<HashSet<Pos>>();
+
+    possible_faces
+        .difference(&cubes.iter().copied().collect::<HashSet<Pos>>())
+        .count()
+}
+
+fn parse(s: &str) -> Vec<Pos> {
+    s.trim()
+        .lines()
+        .map(|l| {
+            let vals: Vec<i32> = l.split(',').map(|s| s.parse::<i32>().unwrap()).collect();
+            Pos::new(vals[0], vals[1], vals[2])
+        })
+        .collect()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn small_example() {
+        assert_eq!(
+            surface_area(&vec![Pos::new(1, 1, 1), Pos::new(2, 1, 1)]),
+            10
+        );
+    }
+
+    const SMALL_INPUT: &str = "\
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
+";
+
+    #[test]
+    fn test_parse() {
+        let positions = parse(SMALL_INPUT);
+        assert_eq!(
+            positions,
+            vec![
+                Pos::new(2, 2, 2),
+                Pos::new(1, 2, 2),
+                Pos::new(3, 2, 2),
+                Pos::new(2, 1, 2),
+                Pos::new(2, 3, 2),
+                Pos::new(2, 2, 1),
+                Pos::new(2, 2, 3),
+                Pos::new(2, 2, 4),
+                Pos::new(2, 2, 6),
+                Pos::new(1, 2, 5),
+                Pos::new(3, 2, 5),
+                Pos::new(2, 1, 5),
+                Pos::new(2, 3, 5),
+            ]
+        );
+    }
+
+    #[test]
+    fn surface_area_small_input() {
+        let positions = parse(SMALL_INPUT);
+        assert_eq!(surface_area(&positions), 64);
+    }
+}
