6f5ae88992d99ec02cd34c345fe48b9de392f5ed
Danny Yoo <dannyyoo@google.com>
2023-11-11 15:09:04 -0800

    Playing with the combinators a bit more.
    I have parsers for the individual atoms; now to try to compose them.

--- day21/src/parser.rs
+++ day21/src/parser.rs
@@ -1,24 +1,67 @@
-use nom::{bytes::complete::tag, character::complete::digit1, IResult};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, char, digit1},
+    combinator::{map, map_res},
+    sequence::Tuple,
+    IResult,
+};
 
-pub enum Term {
-    Number,
-    Variable,
+#[derive(Debug, PartialEq, Eq)]
+pub enum Expr<'a> {
+    Num(i32),
+    Var(&'a str),
 }
 
-fn parse_number(s: &str) -> IResult<&str, &str> {
-    digit1(s)
+#[derive(Debug, PartialEq, Eq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+fn parse_num(s: &str) -> IResult<&str, Expr> {
+    let mut parser = map_res(digit1, |s: &str| s.parse::<i32>().map(Expr::Num));
+    parser(s)
 }
 
 fn parse_colon(s: &str) -> IResult<&str, &str> {
     tag(":")(s)
 }
 
+fn parse_var(s: &str) -> IResult<&str, Expr> {
+    map(alpha1, Expr::Var)(s)
+}
+
+fn parse_op(s: &str) -> IResult<&str, Op> {
+    let parser = alt((char('+'), char('-'), char('*'), char('/')));
+    let mut parser = map(parser, |op: char| match op {
+        '+' => Op::Add,
+        '-' => Op::Sub,
+        '*' => Op::Mul,
+        '/' => Op::Div,
+        _ => panic!(),
+    });
+
+    parser(s)
+}
+
 #[test]
 fn test_parse_number() {
-    assert_eq!(parse_number("42:"), Ok((":", "42")));
+    assert_eq!(parse_num("42:"), Ok((":", Expr::Num(42))));
+}
+#[test]
+fn test_parse_var() {
+    assert_eq!(parse_var("abcd:"), Ok((":", Expr::Var("abcd"))));
 }
 
 #[test]
 fn test_parse_colon() {
     assert_eq!(parse_colon(": 42"), Ok((" 42", ":")));
+}
+
+#[test]
+fn test_parse_op() {
+    assert_eq!(parse_op("+ abcd"), Ok((" abcd", Op::Add)));
 }
