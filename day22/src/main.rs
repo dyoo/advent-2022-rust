3f56cfcab134868a83d3f93b36f14d9a2d1dda5c
Danny Yoo <dannyyoo@google.com>
2023-11-18 12:40:02 -0800

    Initial parsing code done.

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,18 +1,61 @@
+#[derive(Debug, Clone, Copy)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
 #[derive(Debug)]
-struct Problem<'a> {
+struct Problem {
     map: Vec<Vec<char>>,
-    moves: &'a str,
+    moves: Vec<Move>,
+}
+
+#[derive(Debug)]
+enum Move {
+    Forward(usize),
+    Clock,
+    Counterclock,
 }
 
 fn parse_input(s: &str) -> Option<Problem> {
     let mut chunks = s.split("\n\n");
-    let map = chunks
-        .next()?
+    let map = parse_map(chunks.next()?.trim());
+    let moves = parse_moves(chunks.next()?);
+
+    Some(Problem { map, moves })
+}
+
+fn parse_moves(s: &str) -> Vec<Move> {
+    let mut moves = Vec::new();
+    let mut n = 0;
+    for ch in s.trim().chars() {
+        match ch {
+            '0'..='9' => n = n * 10 + (ch as usize - '0' as usize),
+            'L' => {
+                moves.push(Move::Forward(n));
+                moves.push(Move::Counterclock);
+                n = 0;
+            }
+            'R' => {
+                moves.push(Move::Forward(n));
+                moves.push(Move::Clock);
+                n = 0;
+            }
+            _ => {}
+        }
+    }
+    if n != 0 {
+        moves.push(Move::Forward(n));
+    }
+    moves
+}
+
+fn parse_map(s: &str) -> Vec<Vec<char>> {
+    let map = s
         .lines()
         .map(|l| l.chars().collect::<Vec<char>>())
         .collect::<Vec<_>>();
-    let moves = chunks.next()?.trim();
-    Some(Problem { map, moves })
+    map
 }
 
 fn main() {
